f50d8b72656e65c86efb8f532cd0b37ec5bf43a5
hypnotoad08 <ev45blee@gmail.com>
2025-04-15 16:46:03 -0400

    Update Display.ino better functions

--- matter display/Display/Display.ino
+++ matter display/Display/Display.ino
@@ -5,7 +5,10 @@
 Screen_EPD_EXT4_Fast myScreen(eScreen_EPD_290_KS_0F, boardArduinoNanoMatter);
 
 const uint16_t iconSize = 12;
-const uint16_t iconYOffset = 2;  // Visual centering adjustment
+const uint16_t iconYOffset = 2; 
+const uint16_t gridCols = 20;
+const uint16_t gridRows = 12;
+
 
 void drawHeatIcon(uint16_t x, uint16_t y, uint16_t size) {
     myScreen.dLine(x, y, size, 0, myColours.black);
@@ -41,8 +44,20 @@ void drawSensorBar(uint16_t x, uint16_t y, uint16_t width, uint16_t height, int 
     myScreen.dRectangle(x, y, width, height, myColours.black);
 }
 
-void drawWarningLine(const char* message, void (*iconFunc)(uint16_t, uint16_t, uint16_t),
+void drawSensorWithBar(const char* label, int value, int maxValue, uint16_t& y,
+                       uint16_t x, uint16_t barX, uint16_t dx, uint16_t dy)
+{
+    myScreen.gText(x, y, formatString("%s: %d%s", label, value, 
+        strcmp(label, "Humidity") == 0 ? "%" : strcmp(label, "Temp") == 0 ? " F" : ""));
+    drawSensorBar(barX, y, 10 * dx, dy - 4, value, maxValue);
+    y += dy;
+}
+
+
+
+void drawWarningLine(const char* const message, void (*iconFunc)(uint16_t, uint16_t, uint16_t),
                      uint16_t& y, uint16_t dx, uint16_t dy, uint16_t iconX)
+
 {
     myScreen.gText(dx, y, message);
     iconFunc(iconX, y + iconYOffset, iconSize);
@@ -57,10 +72,16 @@ void displayMockSensorData(int temp, int humidity, int aqi) {
     // Grid system
     static uint16_t xMax = myScreen.screenSizeX();
     static uint16_t yMax = myScreen.screenSizeY();
-    uint16_t dx = xMax / 20;
-    uint16_t dy = yMax / 12;
+    const uint16_t dx = xMax / gridCols;
+    const uint16_t dy = yMax / gridRows;
     uint16_t x = dx;
     uint16_t y = dy;
+    uint16_t sensorBarX = x + 7 * dx;
+
+    const int TEMP_THRESHOLD = 85;
+    const int HUMIDITY_THRESHOLD = 70;
+    const int AQI_THRESHOLD = 100;  
+
 
     // Fonts
     myScreen.selectFont(Font_Terminal12x16);
@@ -70,17 +91,10 @@ void displayMockSensorData(int temp, int humidity, int aqi) {
     myScreen.selectFont(Font_Terminal8x12);
 
     // Sensor Readings
-    myScreen.gText(x, y, formatString("Temp: %d F", temp));
-    drawSensorBar(x + 7 * dx, y, 10 * dx, dy - 4, temp, 120);
-    y += dy;
+drawSensorWithBar("Temp", temp, 120, y, x, sensorBarX, dx, dy);
+drawSensorWithBar("Humidity", humidity, 100, y, x, sensorBarX, dx, dy);
+drawSensorWithBar("AQI", aqi, 200, y, x, sensorBarX, dx, dy);
 
-    myScreen.gText(x, y, formatString("Humidity: %d%%", humidity));
-    drawSensorBar(x + 7 * dx, y, 10 * dx, dy - 4, humidity, 100);
-    y += dy;
-
-    myScreen.gText(x, y, formatString("AQI: %d", aqi));
-    drawSensorBar(x + 7 * dx, y, 10 * dx, dy - 4, aqi, 200);
-    y += dy;
     y += dy/2;
 
     // Warning section setup
@@ -88,17 +102,17 @@ void displayMockSensorData(int temp, int humidity, int aqi) {
     int warningCount = 0;
     uint16_t iconX = x + 12 * dx;
 
-    if (temp > 85) {
+    if (temp > TEMP_THRESHOLD) {
         drawWarningLine("Warning: High Temp!", drawHeatWarningIcon, y, x, dy, iconX);
-        warningCount++;
+        ++warningCount;
     }
-    if (humidity > 70) {
+    if (humidity > HUMIDITY_THRESHOLD) {
         drawWarningLine("Warning: High Humidity!", drawHumidityIcon, y, x, dy, iconX);
-        warningCount++;
+        ++warningCount;
     }
-    if (aqi > 100) {
+    if (aqi > AQI_THRESHOLD) {
          drawWarningLine("Warning: Poor AQI!", drawAQIIcon, y, x, dy, iconX);
-        warningCount++;
+        ++warningCount;
     }
 
     // Draw dynamic box around warnings
