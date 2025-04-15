e7400d48912c4d3420dd6d3f780d1ad71ad3c2f9
hypnotoad08 <ev45blee@gmail.com>
2025-04-15 17:10:07 -0400

    Add all Mock Data

--- matter display/Display/Display.ino
+++ matter display/Display/Display.ino
@@ -9,6 +9,37 @@ const uint16_t iconYOffset = 2;
 const uint16_t gridCols = 20;
 const uint16_t gridRows = 12;
 
+struct SensorReadings {
+    int temperatureF;
+    int humidityPercent;
+    int aqi;
+    int vocIndex;
+    int co2ppm;
+    int pm1;
+    int pm25;
+    int pm10;
+};
+
+struct SensorField {
+    const char* label;
+    int value;
+    const char* unit;
+    int maxValue;
+};
+
+SensorReadings generateMockSensorData() {
+    return {
+        .temperatureF = random(65, 95),
+        .humidityPercent = random(40, 90),
+        .aqi = random(20, 160),
+        .vocIndex = random(0, 500),
+        .co2ppm = random(400, 1500),
+        .pm1 = random(0, 100),
+        .pm25 = random(0, 100),
+        .pm10 = random(0, 100),
+    };
+}
+
 
 void drawHeatIcon(uint16_t x, uint16_t y, uint16_t size) {
     myScreen.dLine(x, y, size, 0, myColours.black);
@@ -64,71 +95,70 @@ void drawWarningLine(const char* const message, void (*iconFunc)(uint16_t, uint1
     y += dy;
 }
 
-
-void displayMockSensorData(int temp, int humidity, int aqi) {
+void displaySensorData(const SensorReadings& readings) {
     myScreen.setOrientation(3);
     myScreen.clear();
 
-    // Grid system
-    static uint16_t xMax = myScreen.screenSizeX();
-    static uint16_t yMax = myScreen.screenSizeY();
+    uint16_t xMax = myScreen.screenSizeX();
+    uint16_t yMax = myScreen.screenSizeY();
     const uint16_t dx = xMax / gridCols;
     const uint16_t dy = yMax / gridRows;
     uint16_t x = dx;
     uint16_t y = dy;
     uint16_t sensorBarX = x + 7 * dx;
 
-    const int TEMP_THRESHOLD = 85;
-    const int HUMIDITY_THRESHOLD = 70;
-    const int AQI_THRESHOLD = 100;  
-
-
-    // Fonts
     myScreen.selectFont(Font_Terminal12x16);
     myScreen.gText(x, y, "Air Sensor Readings");
     y += 2 * dy;
-
     myScreen.selectFont(Font_Terminal8x12);
 
-    // Sensor Readings
-drawSensorWithBar("Temp", temp, 120, y, x, sensorBarX, dx, dy);
-drawSensorWithBar("Humidity", humidity, 100, y, x, sensorBarX, dx, dy);
-drawSensorWithBar("AQI", aqi, 200, y, x, sensorBarX, dx, dy);
+    // Display sensor values
+    auto drawSensorWithBar = [&](const char* label, int value, int max) {
+        myScreen.gText(x, y, formatString("%s: %d", label, value));
+        drawSensorBar(sensorBarX, y, 10 * dx, dy - 4, value, max);
+        y += dy;
+    };
 
-    y += dy/2;
+    drawSensorWithBar("Temp (F)", readings.temperatureF, 120);
+    drawSensorWithBar("Humidity (%)", readings.humidityPercent, 100);
+    drawSensorWithBar("AQI", readings.aqi, 200);
+    drawSensorWithBar("PM1", readings.pm1, 200);
+    drawSensorWithBar("PM2.5", readings.pm25, 200);
+    drawSensorWithBar("PM10",readings.pm10,200);
+    drawSensorWithBar("CO2 (ppm)", readings.co2ppm, 2000);
+    drawSensorWithBar("VOC Index", readings.vocIndex, 500);
 
-    // Warning section setup
+    y += dy / 2;
+
+    // Warnings
     uint16_t warningStartY = y;
     int warningCount = 0;
     uint16_t iconX = x + 12 * dx;
 
-    if (temp > TEMP_THRESHOLD) {
+    if (readings.temperatureF > 85) {
         drawWarningLine("Warning: High Temp!", drawHeatWarningIcon, y, x, dy, iconX);
         ++warningCount;
     }
-    if (humidity > HUMIDITY_THRESHOLD) {
+    if (readings.humidityPercent > 70) {
         drawWarningLine("Warning: High Humidity!", drawHumidityIcon, y, x, dy, iconX);
         ++warningCount;
     }
-    if (aqi > AQI_THRESHOLD) {
-         drawWarningLine("Warning: Poor AQI!", drawAQIIcon, y, x, dy, iconX);
+    if (readings.aqi > 100) {
+        drawWarningLine("Warning: Poor AQI!", drawAQIIcon, y, x, dy, iconX);
         ++warningCount;
     }
 
-    // Draw dynamic box around warnings
     if (warningCount > 0) {
         uint16_t boxX = x - 4;
         uint16_t boxY = warningStartY - 2;
         uint16_t boxWidth = 17.3 * dx;
         uint16_t boxHeight = warningCount * dy + 6;
-
         myScreen.setPenSolid(false);
         myScreen.dRectangle(boxX, boxY, boxWidth, boxHeight, myColours.black);
     }
 
     myScreen.flush();
 }
-
 
 void setup() {
     mySerial.begin(115200);
@@ -139,9 +169,8 @@ void setup() {
 }
 
 void loop() {
-    int temp = random(65, 95);
-    int humidity = random(40, 90);
-    int aqi = random(20, 160);
-    displayMockSensorData(temp, humidity, aqi);
+    SensorReadings mock = generateMockSensorData();
+    displaySensorData(mock);
+    //displayMockSensorData(temp, humidity, aqi);
     delay(10000);
 }
