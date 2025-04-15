7f32fea8afb1452407741ae11e2083f0e2a7544d
hypnotoad08 <ev45blee@gmail.com>
2025-04-15 00:19:10 -0400

    icon alignment

--- matter display/Display/Display.ino
+++ matter display/Display/Display.ino
@@ -2,156 +2,123 @@
 #include "hV_HAL_Peripherals.h"
 
 Screen_EPD_EXT4_Fast myScreen(eScreen_EPD_290_KS_0F, boardArduinoNanoMatter);
+const uint16_t warningIconSize = 12;
 
-void drawWarningTriangleWithDot(uint16_t x, uint16_t y, uint16_t size) {
-    myScreen.setPenSolid(false);
-    uint16_t midX = x + size / 2; // Store the repeated calculation in a local variable
-    myScreen.triangle(midX, y, x, y + size, x + size, y + size, myColours.black);
-     myScreen.setPenSolid(true); // Set to draw a filled circl
-    myScreen.circle(midX, y + size * 2 / 3, size / 10, myColours.black);  // Little dot as alert
-}
-
-void drawDropletIcon(uint16_t x, uint16_t y, uint16_t size) {
-    uint16_t centerX = x + size / 2;
-    uint16_t topY = y;
-    uint16_t bottomY = y + size;
-
-    myScreen.setPenSolid(false);
-
-    // Pointed top (triangle part of the droplet)
-    myScreen.triangle(
-        centerX, topY,                 // top point
-        x, bottomY - size / 3,        // bottom left
-        x + size, bottomY - size / 3, // bottom right
-        myColours.black
-    );
-
-    // Rounded bottom (circle part of the droplet)
-    myScreen.circle(centerX, bottomY - size / 4, size / 4, myColours.black);
-}
-
-void drawHeatIcon(uint16_t x, uint16_t y, uint16_t width) {
-    myScreen.dLine(x, y, width / 2, 0, myColours.black);
-    myScreen.dLine(x, y + 3, width, 0, myColours.black);
-    myScreen.dLine(x, y + 6, width / 2, 0, myColours.black);
-}
-
-void drawHumidityIcon(uint16_t x, uint16_t y, uint16_t size) {
-    // Drop tip (triangle)
-    myScreen.triangle(
-        x + size / 2, y,
-        x, y + size,
-        x + size, y + size,
-        myColours.black
-    );
-    // Drop bottom (circle overlap)
-    myScreen.circle(x + size / 2, y + size, size / 4, myColours.black);
-}
-
-
-void drawThermometerIcon(uint16_t x, uint16_t y, uint16_t height) {
-    uint16_t bulbRadius = height / 5;
-    uint16_t stemHeight = height - bulbRadius * 2;
-    uint16_t stemWidth = 2;
-
-    uint16_t centerX = x + bulbRadius;
-    uint16_t topY = y;
-    uint16_t bulbY = y + stemHeight;
-
-    myScreen.setPenSolid(true);
-
-    // Draw stem as a vertical thin rectangle
-    myScreen.dRectangle(centerX - stemWidth / 2, topY, stemWidth, stemHeight, myColours.black);
-
-    // Draw bulb as a circle
-    myScreen.circle(centerX, bulbY + bulbRadius, bulbRadius, myColours.black);
-}
-
-void drawSensorBar(uint16_t x, uint16_t y, uint16_t width, uint16_t height, int value, int maxValue) {
-    value = constrain(value, 0, maxValue); // Ensure value is within range
+void drawFramedBar(uint16_t x, uint16_t y, uint16_t width, uint16_t height, int value, int maxValue)
+{
+    value = constrain(value, 0, maxValue);
     uint16_t barWidth = map(value, 0, maxValue, 0, width);
 
+    // Frame outline
+    myScreen.setPenSolid(false);
+    myScreen.dRectangle(x, y, width, height, myColours.black);
+
+    // Filled bar
     myScreen.setPenSolid(true);
     myScreen.dRectangle(x, y, barWidth, height, myColours.black);
 }
 
+void drawWarningTriangle(uint16_t x, uint16_t y, uint16_t size)
+{
+    myScreen.setPenSolid(false);
+    myScreen.triangle(
+        x + size / 2, y,
+        x, y + size,
+        x + size, y + size,
+        myColours.black);
+}
 
+void drawHeatWaveIcon(uint16_t x, uint16_t y)
+{
+    myScreen.setPenSolid(true);
+    for (int i = 0; i < 3; i++) {
+        myScreen.dLine(x, y + i * 4, 6, 0, myColours.black);
+    }
+}
 
-const uint16_t warningIconSize = 12; // Define a descriptive constant for the warning icon size
+void drawHumidityDroplet(uint16_t x, uint16_t y)
+{
+    myScreen.setPenSolid(false);
+    myScreen.circle(x + 6, y + 6, 6, myColours.black);
+    myScreen.dLine(x + 6, y, 0, 6, myColours.black);
+}
 
-void displayMockSensorData(int temp, int humidity, int aqi) {
+void displayMockSensorData(int temp, int humidity, int aqi)
+{
     myScreen.setOrientation(3);
     myScreen.clear();
 
-    // Grid dimensions
-    static uint16_t xMax = myScreen.screenSizeX();
-    static uint16_t yMax = myScreen.screenSizeY();
-    uint16_t dx = xMax / 20; // horizontal unit
-    uint16_t dy = yMax / 12; // vertical unit
+    uint16_t xMax = myScreen.screenSizeX();
+    uint16_t yMax = myScreen.screenSizeY();
+    uint16_t dx = xMax / 20;
+    uint16_t dy = yMax / 12;
     uint16_t x = dx;
     uint16_t y = dy;
 
-    // Fonts
     myScreen.selectFont(Font_Terminal12x16);
     myScreen.gText(x, y, "Air Sensor Readings");
     y += 2 * dy;
 
     myScreen.selectFont(Font_Terminal8x12);
+    uint16_t barX = x + 7 * dx;
+    uint16_t barW = 10 * dx;
+    uint16_t barH = dy - 2;
 
     // Temp
     myScreen.gText(x, y, formatString("Temp: %d F", temp));
-    drawSensorBar(x + 7 * dx, y, 10 * dx, dy - 2, temp, 120);
+    drawFramedBar(barX, y, barW, barH, temp, 120);
     y += dy;
 
     // Humidity
     myScreen.gText(x, y, formatString("Humidity: %d%%", humidity));
-    drawSensorBar(x + 7 * dx, y, 10 * dx, dy - 2, humidity, 100);
+    drawFramedBar(barX, y, barW, barH, humidity, 100);
     y += dy;
 
     // AQI
     myScreen.gText(x, y, formatString("AQI: %d", aqi));
-    drawSensorBar(x + 7 * dx, y, 10 * dx, dy - 2, aqi, 200);
+    drawFramedBar(barX, y, barW, barH, aqi, 200);
     y += dy;
 
     // Warnings
     y += dy / 2;
+    uint16_t iconX = xMax - (warningIconSize + dx);
+    uint16_t iconY = y;
+
     if (temp > 85) {
-        myScreen.gText(x, y, "Warning: High Temp!");
-        drawHeatIcon(x + 13 * dx, y + 2, 10);
-        y += dy;
+        myScreen.gText(x, iconY, "Warning: High Temp!");
+        drawHeatWaveIcon(iconX, iconY);
+        iconY += dy;
     }
+
     if (humidity > 70) {
-        myScreen.gText(x, y, "Warning: High Humidity!");
-        drawHumidityIcon(x + 13 * dx, y, warningIconSize);
-        y += dy;
+        myScreen.gText(x, iconY, "Warning: High Humidity!");
+        drawHumidityDroplet(iconX, iconY);
+        iconY += dy;
     }
+
     if (aqi > 100) {
-        drawWarningTriangleWithDot(x + 12 * dx, y, warningIconSize);
-        myScreen.gText(x, y, "Warning: Poor AQI!");
-        y += dy;
+        myScreen.gText(x, iconY, "Warning: Poor AQI!");
+        drawWarningTriangle(iconX, iconY, warningIconSize);
+        iconY += dy;
     }
 
     myScreen.flush();
 }
 
-void setup() {
-
+void setup()
+{
     mySerial.begin(115200);
-    delay(500); // Allow time for the serial connection to stabilize
-
-    // Initialize the screen and prepare it for use
+    delay(500);
     myScreen.begin();
-    myScreen.regenerate(); // Refresh the screen to ensure a clean state
-
-    // Seed the random number generator using an analog pin
+    myScreen.regenerate();
     randomSeed(analogRead(A0));
 }
 
-void loop() {
+void loop()
+{
     int temp = random(65, 95);
     int humidity = random(40, 90);
     int aqi = random(20, 160);
-
     displayMockSensorData(temp, humidity, aqi);
     delay(10000);
 }
